3a90a7b2661ad67622b6678467a44fe12cd9f891
sanjay-ak-us <sanjay.ak.us@gmail.com>
2015-01-05 12:27:19 +0100

    Assignment Unit3-Lesson1-2

--- FibonacciApp/ViewController.swift
+++ FibonacciApp/ViewController.swift
@@ -6,10 +6,23 @@
 //  Copyright (c) 2015 sanjay. All rights reserved.
 //
 
-import UIKit
+import UIKit;
 
 class ViewController: UIViewController {
 
+    var fibonacciSequence = FibonacciSequence(numberOfItemsInSequence: 12, includesZero: true);
+    
+    @IBOutlet weak var textView: UITextView!;
+    
+    @IBOutlet weak var includesZeroSwitch: UISwitch!;
+    
+    @IBOutlet weak var includesZeroLabel: UILabel!;
+    
+    
+    @IBOutlet weak var numberOfItemsLabel: UILabel!;
+    
+    @IBOutlet weak var numberOfItemsSlider: UISlider!;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +33,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func updateFibonacciSequence(sender: AnyObject, forEvent event: UIEvent) {
+        var totalItems:UInt = UInt(numberOfItemsSlider.value);
+        var zero:Bool = includesZeroSwitch.on;
+        fibonacciSequence = FibonacciSequence(numberOfItemsInSequence: totalItems, includesZero: zero);
+        if(zero){
+            includesZeroLabel.text = "Yes";
+        }
+        else{
+            includesZeroLabel.text = "No";
+        }
+        numberOfItemsLabel.text = String(totalItems);
+        textView.text = fibonacciSequence.values.description;
+    }
 
 }
 
